15732d066ccd2fb24c21e5af611225fbf720616f
na0260 <samjr.008@gmail.com>
2024-10-02 01:04:23 +0600

    all pet show and single pet show api endpoint added

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class PetController extends Controller
 {
-    public function addPet(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $user = auth('api')->user();
+        $pets = Pet::with('shelter','petImages')->get();
+        $count = $pets->count();
+        return response()->json([
+            'status' => 'success',
+            'total_pets' => $count,
+            'data' => $pets,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user = JWTAuth::authenticate();
 
         if ($user->role === 'user') {
             return response()->json([
@@ -76,6 +95,41 @@ class PetController extends Controller
                 'message' => $e->getMessage()
             ], 500);
         }
+    }
 
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $pet = Pet::with('shelter','petImages')->find($id);
+
+        if (!$pet) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Pet not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $pet
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
