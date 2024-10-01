669823e10e5a8a4b54111d14efc1a454fb4cb807
na0260 <samjr.008@gmail.com>
2024-10-02 01:04:18 +0600

    all pet show and single pet show api endpoint added

--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,28 @@ Route::group([
     'middleware' => 'api',
     ], function () {
     Route::group(['prefix' => 'admin', 'middleware' => JwtMiddleware::class], function () {
-        Route::get('/users', [AdminController::class, 'getUser']);
-        Route::get('/shelters', [AdminController::class, 'getShelter']);
+        Route::get('/users', [AdminController::class, 'getUser'])->name('get.users');
+        Route::get('/shelters', [AdminController::class, 'getShelter'])->name('get.shelters');
     });
     Route::group(['prefix' => 'auth'], function () {
         Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
         Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1')->name('auth.login');
         Route::group(['middleware' => JwtMiddleware::class], function () {
-            Route::post('/logout', [AuthController::class, 'logout']);
-            Route::get('/me', [AuthController::class, 'me']);
+            Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+            Route::get('/me', [AuthController::class, 'me'])->name('auth.me');
         });
     });
-    Route::post('/send-otp', [PasswordResetController::class, 'sendOTP'])->middleware('throttle:3,1');
-    Route::post('/validate-otp', [PasswordResetController::class, 'validateOTP']);
-    Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
+    Route::post('/send-otp', [PasswordResetController::class, 'sendOTP'])->middleware('throttle:3,1')->name('send.otp');
+    Route::post('/validate-otp', [PasswordResetController::class, 'validateOTP'])->name('validate.otp');
+    Route::post('/reset-password', [PasswordResetController::class, 'resetPassword'])->name('reset.password');
 
     Route::group(['prefix' => 'shelter'], function () {
-        Route::post('/register', [ShelterController::class, 'createShelter']);
-        Route::put('/update', [ShelterController::class, 'updateShelter'])->middleware(JwtMiddleware::class);
-        Route::group(['prefix' => 'pet'], function () {
-            Route::post('/add', [PetController::class, 'addPet'])->middleware(JwtMiddleware::class);
-        });
+        Route::post('/register', [ShelterController::class, 'createShelter'])->name('shelter.register');
+        Route::put('/update', [ShelterController::class, 'updateShelter'])->middleware(JwtMiddleware::class)->name('shelter.update');
+    });
+    Route::apiResource('/pets', PetController::class)->only(['index', 'show']);
+    Route::group(['prefix' => 'pet','middleware'=>JwtMiddleware::class], function () {
+        Route::post('/add', [PetController::class, 'addPet'])->name('pet.add');
     });
 });
 
