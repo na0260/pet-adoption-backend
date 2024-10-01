b87395c2544b0b1df94e9f011110349a770bcd90
na0260 <samjr.008@gmail.com>
2024-10-01 22:08:36 +0600

    Pets table updated

---
+++ database/migrations/2024_10_01_214934_change_health_condition_column_in_pets_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pets', function (Blueprint $table) {
+            $table->enum('health_condition', ['healthy', 'sick', 'injured'])->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pets', function (Blueprint $table) {
+            $table->string('health_condition')->nullable()->change();
+        });
+    }
+};
